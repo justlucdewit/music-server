4bcf5ae0ddea64f5c83fbc97814c6bee8ad17ebf
skills <skills@LAS-459462.formation.forem>
2022-11-15 13:54:39 +0100

    css endpoint

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const uuid = require('uuid');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
+const sessions = [];
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    console.log(__dirname + '\index.html')
     res.sendFile(__dirname + '\\frontend\\src\\index.html');
+});
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '\\frontend\\src\\style.css');
+});
+
+app.post('/login', (req, res) => {
+    const userId = uuid.v4();
+    const user = {
+        id: userId,
+        name: req.body.user
+    }
+
+    res.send(user);
+})
+
+io.on('connection', (socket) => {
+    socket.on('play_note', msg => {
+        console.log('[play_note] message: ' + msg);
+    });
 });
 
 server.listen(3000, () => {
