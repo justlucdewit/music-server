c39c74d2c18333af7411053fb70e199d15a3f744
skills <skills@LAS-459462.formation.forem>
2022-11-15 15:49:33 +0100

    port 8081

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,3 +3,6 @@ const login = async () => {
     const result = (await axios.post('/login', { name: username })).data;
     document.location.href = '/music.html?uid=' + result.id;
 }
+
+const urlParams = new URLSearchParams(window.location.search);
+window.userid = urlParams.get('uid')
